c0c91e9326aca91a96c225665062790cf81cb891
akbarrt <albarnv10@gmail.com>
2025-03-12 00:01:47 +0700

    insert file main.java

---
+++ main.java
@@ -0,0 +1,19 @@
+
+public class ayam {
+	
+    void berkokok() {
+        System.out.println("Ayam telah berkokok cukurukuk");
+    }
+
+    void bertelur() {
+        System.out.println("Ayam sedang mengerami telur-telurnya");
+    }
+
+    public static void main(String[] args) {
+        ayam ayamm = new ayam();
+        ayam ayamm = new ayam();
+        ayamm.berkokok();
+        ayamm.bertelur(); 
+    }
+}
+
